791691e26821c8b5d1e94c8e2de4c20c83fbe1f9
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-12-01 10:22:58 -0300

    fix study schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -2,10 +2,15 @@ def study_schedule(permanence_period, target_time):
 
     if target_time is None:
         return None
-
     contador = 0
-    for entrada, saida in permanence_period:
-        if entrada <= target_time < saida:
-            contador += 1
+    if not all(
+        isinstance(start, int) and isinstance(end, int)
+        for start, end in permanence_period
+    ):
+        return None
+    for start, end in permanence_period:
+      if start <= target_time <= end:
+         contador += 1
+    
 
     return contador
