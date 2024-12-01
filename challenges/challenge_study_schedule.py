b560bef3978b4a2b7f84d9c0cf0d72c50750eebe
Felipe Netuno <FELIPEMANOEL916@GMAIL.COM>
2024-12-01 09:43:55 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+
+    if target_time is None:
+        return None
+
+    contador = 0
+    for entrada, saida in permanence_period:
+        if entrada <= target_time < saida:
+            contador += 1
+
+    return contador
